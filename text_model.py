ae94a8c462cc2e37745f0c0337e8267c22c9b368
Ambar Chakraborty <ambarchakra@gmail.com>
2023-01-27 23:22:56 -0500

    Adding text model

--- text_model.py
+++ text_model.py
@@ -5,10 +5,13 @@ co = cohere.Client('DLHq9Go9DHorXORElej8QEbINQZYq3NrM0G3RsGo')
 
 inputs = ["beach ball"]
 
+examples = []
+
 response = co.classify(
     model='large',
     inputs=inputs,
     examples=examples)
+
 
 # print("Formatted version: {}".format(response.classifications)[0])
 # print("Formatted version: {}".response.classifications[0])
